267589ff8a767ada546cdfca3a140d6009defbef
Maito-official <111188232+Maito-official@users.noreply.github.com>
2022-11-28 06:11:35 -0800

    Add files via upload

---
+++ Pepsi Landing Page/index.js
@@ -0,0 +1,21 @@
+
+document.querySelector(".good").addEventListener('click',function(){
+    document.querySelector(".pepsi").src = "pepsi001.png";
+    document.querySelector('.sec').style.background = '#0062be'
+})
+
+document.querySelector(".bad").addEventListener('click',function(){
+    document.querySelector(".pepsi").src = "pepsi002.png";
+    document.querySelector('.sec').style.background ='#e60c2c'  
+})
+document.querySelector(".bee").addEventListener('click',function(){
+    document.querySelector(".pepsi").src = "pepsi003.png";
+    document.querySelector('.sec').style.background = '#1e1e1e' 
+})
+
+   
+
+
+
+
+
